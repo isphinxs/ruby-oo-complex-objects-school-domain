75fcdfe451987f00777d97926480dba2cfd951e6
Samantha Ostrowski <isphinxs@hotmail.com>
2020-11-24 21:04:09 -0500

    commit tree 78554982876b8b108d985b7911f01410a4d2b6e3

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+require "pry"
+class School
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        if @roster.has_key?(grade)
+            @roster[grade] << name
+        else
+            @roster[grade] = []
+            @roster[grade] << name
+        end 
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        # first try
+        # temp_roster = {}
+        # @roster.collect do |grade, students|
+        #     temp_roster[grade] = students.sort
+        # end
+        # temp_roster
+
+        @roster.collect {|grade, students| [grade, students.sort]}.to_h
+    end
+end
